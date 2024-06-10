328f7d6379c3595b8a0555e54609ce682dcb7929
Deenesh Handol <166477580+Deeneshhhandol@users.noreply.github.com>
2024-06-10 03:22:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-// CALCULATOR PROGRAM
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-const display = document.getElementById("display");
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
 
-function appendToDisplay(input){
-    display.value += input;
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
 
-function clearDisplay(){
-    display.value = "";
-}
-
-function calculate(){
-    try{
-        display.value = eval(display.value);
-    }
-    catch(error){
-        display.value = "Error";
-    }
-}
+    })
+})
